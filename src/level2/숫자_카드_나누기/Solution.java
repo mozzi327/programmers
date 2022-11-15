aabe799f6921e9daffd30f451a856a8584668b49
mozzi327 <mozzi327@gmail.com>
2022-11-15 09:40:46 +0900

    level2 : 연속_부분_수열_합의_개수

--- src/level2/숫자_카드_나누기/Solution.java
+++ src/level2/숫자_카드_나누기/Solution.java
@@ -1,2 +1,56 @@
-package level2.숫자_카드_나누기;public class Solution {
+package level2.숫자_카드_나누기;
+
+import java.util.*;
+
+public class Solution {
+    public static int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+
+        int minA = Arrays.stream(arrayA).min().getAsInt();
+        int minB = Arrays.stream(arrayB).min().getAsInt();
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = 2; i <= minA; i++) {
+            if(minA % i == 0) set.add(i);
+        }
+
+        for(int i = 2; i <= minB; i++) {
+            if(minB % i == 0) set.add(i);
+        }
+
+        List<Integer> list = new ArrayList<>(set);
+
+        System.out.println(list);
+
+        boolean isPossible;
+        for (Integer integer : list) {
+            isPossible = true;
+            for (int j = 0; j < arrayA.length; j++) {
+                if (arrayA[j] % integer != 0 || arrayB[j] % integer == 0) {
+                    isPossible = false;
+                    break;
+                }
+            }
+
+            if (isPossible) answer = Math.max(answer, integer);
+
+            isPossible = true;
+
+            for (int j = 0; j < arrayB.length; j++) {
+                if (arrayB[j] % integer != 0 || arrayA[j] % integer == 0) {
+                    isPossible = false;
+                    break;
+                }
+            }
+
+            if (isPossible) answer = Math.max(answer, integer);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{10, 20}, new int[]{5, 17}));
+    }
 }
--- src/level2/연속_부분_수열_합의_개수/Solution.java
+++ src/level2/연속_부분_수열_합의_개수/Solution.java
@@ -1,2 +1,27 @@
-package level2.연속_부분_수열_합의_개수;public class Solution {
+package level2.연속_부분_수열_합의_개수;
+
+import java.util.*;
+
+class Solution {
+    public static int solution(int[] elements) {
+        HashSet<Integer> resultSet = new HashSet<>();
+        int N = elements.length;
+        int[] powElements = new int[N * 2];
+        for(int i = 0; i < N; i++) {
+            powElements[i] = powElements[i + N] = elements[i];
+        }
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                resultSet.add(Arrays.stream(powElements, j, j+i).sum());
+            }
+        }
+
+
+        return resultSet.size();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{7,9,1,1,4})); // 18
+    }
 }
